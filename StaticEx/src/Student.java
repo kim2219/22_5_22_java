3ca1888ea15567ad58c0c988d1b8e9b1ed82362b
kimrisa <h1010210@gmail.com>
2022-05-16 15:22:05 +0900

    [추가]:StaticEx

---
+++ StaticEx/src/Student.java
@@ -0,0 +1,12 @@
+
+public class Student {
+	static int serialNum = 1000;
+	
+	int studnetID;
+	String studentName;
+	
+	public static void AddSerialNum() {
+		serialNum++;
+	}
+
+}
---
+++ StaticEx/src/main.java
@@ -0,0 +1,17 @@
+
+public class main {
+	public static void main(String[] args){
+		
+		Student studnet1 = new Student();
+		Student studnet2 = new Student();
+		
+		
+	    Student.serialNum++;
+		
+		System.out.println(studnet1.serialNum);
+		System.out.println(studnet2.serialNum);
+		
+	}
+	
+
+}
