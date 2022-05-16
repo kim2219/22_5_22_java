dfd00ddc0027ae66df4ef946b69a0d7c882d78db
kimrisa <h1010210@gmail.com>
2022-05-16 15:27:27 +0900

    [추가]프로젝트1

---
+++ project1/src/project1/Person.java
@@ -0,0 +1,25 @@
+package project1;
+
+public class Person {
+	
+	
+	String name;
+	int age;
+	int height;
+	int weight;
+	
+	public Person() {
+		this.name = name;
+		this.age = age;
+		this.height = height;
+		this.weight = weight;
+		
+	}
+	public void showInfi() {
+		System.out.println(name);
+		System.out.println(age);
+		System.out.println(height);
+		System.out.println(weight);
+	}
+
+}
---
+++ project1/src/project1/main.java
@@ -0,0 +1,15 @@
+package project1;
+
+public class main {
+	public static void main(String[] args) {
+		System.out.println("프로그램 시작!");
+		Person person1 = new Person();
+		Person person2 = new Person();
+		
+		System.out.println();
+		
+		
+		
+		
+	}
+}
